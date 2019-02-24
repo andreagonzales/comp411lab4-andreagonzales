f96c7e20f9b553730d9d639f8e8a7505c1779421
Andrea Gonzales <andrea.gonzales@unc.edu>
2019-02-24 21:09:56 +0000

    added ex3 file and did ex1!

--- ex1.c
+++ ex1.c
@@ -4,7 +4,7 @@
 #include <string.h> /* Need for strlen() */
 
 
-#define NUM 25   /* number of strings */
+#define NUM 2   /* number of strings */
 #define LEN 1000  /* max length of each string */
 
 
@@ -25,9 +25,18 @@ int my_compare_strings(char string1[], char string2[]) {
       of the characters in the string, from left to right.  See the writeup
       for more details.
   */
+    for (int i = 0; i < LEN; i++) {
+        if (string1[i] > string2[i]) {
+            return 1;
+        } else if (string2[i] > string1[i]) {
+            return -1;
+        }
+    }
+    return 0;
 }
 
-
+//string 1 -> string 2
+//string 2 -> string 1
 void my_swap_strings(char string1[], char string2[]) {
   char temp;    // char variable used in swapping one character at a time
 
@@ -37,6 +46,17 @@ void my_swap_strings(char string1[], char string2[]) {
      The max length of the strings is LEN, though they may be shorter, so look
      for the string terminators.
   */
+    for (int i = 0; i < LEN; i++) {
+        //if(strlen(string2) > i) {
+        //    temp = string2[i];
+        //}
+        //if(strlen(string1) > i) {
+        //    string2[i] = string1[i];
+        //}
+        temp = string2[i];
+        string2[i] = string1[i];
+        string1[i] = temp;
+    }
 }
 
 
@@ -47,16 +67,21 @@ int main()
   printf("Please enter %d strings, one per line:\n", NUM);
 
   /* Write a for loop here to read NUM strings.
-
      Use fgets(), with LEN as an argument to ensure that an input line that is too
      long does not exceed the bounds imposed by the string's length.  Note that the
      newline and NULL characters will be included in LEN.
   */
+  for (int i = 0 ; i < NUM; i++) {
+     fgets(Strings[i], LEN, stdin);
+  }
+  
 
   puts("\nHere are the strings in the order you entered:");
 
   /* Write a for loop here to print all the strings. */
-
+    for (int i = 0; i < NUM; i++) {
+        printf("%s", Strings[i]);
+    }
   
   /* Bubble sort */
   /* Write code here to bubble sort the strings in ascending alphabetical order
@@ -65,14 +90,22 @@ int main()
      use the function my_swap_strings() to swap their contents.
 
   */
+    for (int i = 0; i < NUM-1; i++) {
+        for (int j = 0; j < NUM-i-1; j++) {
+            if(my_compare_strings(Strings[j], Strings[j+1]) == 1) {
+                my_swap_strings(Strings[j], Strings[j+1]);
+            }
+        }
+    }
 
-  
-  
   /* Output sorted list */
-  
+
   puts("\nIn alphabetical order, the strings are:");     
   /* Write a for loop here to print all the strings. Feel free to use puts/printf
      etc. for printing each string.
   */
+  for (int i = 0; i < NUM; i++) {
+      printf("%s", Strings[i]);
+  }
 
 }
